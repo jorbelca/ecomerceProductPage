6cfceec62affb0cbc5f7f7b1f7a3da68fb02e1ad
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-10-06 18:24:43 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+import ProductPage from "./ProductPage"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ProductPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/jorbelca" target="_blank" rel="noreferrer">
+          Jordi Belda
+        </a>
+        .
+      </div>
+    </footer>
+  )
+}
